455b9404059f966d45cf4b09edb0d09f8761cfc7
Guerro323 <kaltobattle@gmail.com>
2023-12-13 00:24:13 +0100

    first version of specular occlusion

--- servers/rendering_server.cpp
+++ servers/rendering_server.cpp
@@ -3648,6 +3648,7 @@ void RenderingServer::init() {
 	GLOBAL_DEF(PropertyInfo(Variant::INT, "rendering/limits/global_shader_variables/buffer_size", PROPERTY_HINT_RANGE, "16,1048576,1"), 65536);
 
 	GLOBAL_DEF(PropertyInfo(Variant::FLOAT, "rendering/lightmapping/probe_capture/update_speed", PROPERTY_HINT_RANGE, "0.001,256,0.001"), 15);
+	GLOBAL_DEF_RST(PropertyInfo(Variant::INT, "rendering/lightmapping/specular_occlusion/mode", PROPERTY_HINT_ENUM, "Disabled (Fastest),Reduce (Faster),Conservative (Fast)"), 2);
 	GLOBAL_DEF(PropertyInfo(Variant::FLOAT, "rendering/lightmapping/primitive_meshes/texel_size", PROPERTY_HINT_RANGE, "0.001,100,0.001"), 0.2);
 	GLOBAL_DEF("rendering/lightmapping/lightmap_gi/use_bicubic_filter", true);
 
